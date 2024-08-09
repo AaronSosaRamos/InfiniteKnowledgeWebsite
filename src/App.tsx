3f4e83db7919e5a211d11a4ed4983b2a49a519b0
WilfredoAaronSosaRamosBoushtech <wilfredo.sosa@boushtech.net>
2024-08-09 09:01:11 -0500

    new result section imp

--- src/App.tsx
+++ src/App.tsx
@@ -2,12 +2,14 @@ import React from 'react';
 import MainLayout from './layouts/MainLayout';
 import MainBanner from './components/MainBanner';
 import ServicesSection from './components/ServicesSection';
+import ResultsSection from './components/ResultsSection';
 
 const Home: React.FC = () => {
     return (
         <MainLayout>
             <MainBanner />
             <ServicesSection />
+            <ResultsSection />
         </MainLayout>
     );
 };
